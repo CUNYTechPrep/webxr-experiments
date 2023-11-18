d2ad766b2c6ca54f21a7e0976775b34f0b12b0e6
xchen <xchen601@gmail.com>
2023-11-18 17:48:15 -0500

    added second navigation behind user, unclickable

--- data/config.js
+++ data/config.js
@@ -12,13 +12,24 @@ const house = {
                 {
                     "whereTo": "QCKeilyHall3",
                     "type": "Nav",
-                    "location": [-0.1, 1.4, -3], //[x, y, z],
-                    "rotation": [0, 0, 90], //[x, y, z],
+                    "location": [.1, 1.4, -4], //[x, y, z],
+                    "rotation": [-45, 0, 90], //[x, y, z],
                     "width": "0.7",
                     "height": "0.7",
                     "distance": 1,
                     "originalRoom": "Entry",
                     "id": 1789
+                },
+                {
+                    "whereTo": "QCKeilyHall2",
+                    "type": "Nav",
+                    "location": [.1, 1, 4], //[x, y, z],
+                    "rotation": [-45, 0, 90], //[x, y, z],
+                    "width": "0.7",
+                    "height": "0.7",
+                    "distance": 1,
+                    "originalRoom": "Entry",
+                    "id": 1790
                 },
                 {
                     "whereTo": "Entry",
